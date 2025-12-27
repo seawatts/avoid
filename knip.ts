abe71ae437bc9cc3322fb12e507f1fcdf7be8804
Chris Watts <chris.watts.t@gmail.com>
2025-12-26 21:58:42 -0800

    refactor(knip): simplify config with wildcards and add package.json to ignore

--- knip.ts
+++ knip.ts
@@ -1,7 +1,12 @@
 import type { KnipConfig } from 'knip';
 
 const config: KnipConfig = {
-  ignore: ['**/drizzle.config.ts', '**/metro.config.js', '**/babel.config.js'],
+  ignore: [
+    '**/drizzle.config.ts',
+    '**/metro.config.js',
+    '**/babel.config.js',
+    './package.json',
+  ],
   ignoreDependencies: [
     '@vercel/analytics',
     '@vercel/config',
@@ -33,7 +38,6 @@ const config: KnipConfig = {
   ],
   ignoreExportsUsedInFile: true,
   ignoreWorkspaces: [
-    'apps/expo',
     'apps/ios',
     'tooling/next',
     'tooling/commitlint',
@@ -49,45 +53,17 @@ const config: KnipConfig = {
     '.': {
       entry: 'checkly.config.ts',
     },
-    'apps/chrome-extension': {
-      entry: ['src/**/*.tsx', 'src/**/*.ts'],
-      project: ['src/**/*.ts', 'src/**/*.tsx'],
-    },
-    'apps/cli': {
-      entry: ['src/**/*.tsx', 'src/**/*.ts'],
-      project: ['src/**/*.ts', 'src/**/*.tsx'],
-    },
-    'apps/vscode-extension': {
-      entry: ['src/**/*.tsx', 'src/**/*.ts'],
-      project: ['src/**/*.ts', 'src/**/*.tsx'],
-    },
-    'apps/web-app': {
-      entry: ['src/**/*.tsx', 'src/**/*.ts'],
+    'apps/*': {
+      entry: ['src/**/*.ts', 'src/**/*.tsx'],
       project: ['src/**/*.ts', 'src/**/*.tsx'],
     },
     'packages/*': {
       entry: ['src/**/*.ts', 'src/**/*.tsx'],
       project: ['src/**/*.ts', 'src/**/*.tsx'],
     },
-    'packages/auth': {
-      entry: ['src/**/*.ts'],
-      project: ['src/**/*.ts'],
-    },
-    'packages/id': {
-      entry: ['src/**/*.ts'],
-      project: ['src/**/*.ts'],
-    },
     'packages/integ-test': {
       entry: ['src/**/*.ts', 'test-utils/**/*.ts', 'vitest.config.ts'],
       project: ['src/**/*.ts', 'test-utils/**/*.ts'],
-    },
-    'packages/logger': {
-      entry: ['src/**/*.ts'],
-      project: ['src/**/*.ts'],
-    },
-    'packages/utils': {
-      entry: ['src/**/*.ts'],
-      project: ['src/**/*.ts'],
     },
     'tooling/testing': {
       entry: ['src/**/*.ts'],
